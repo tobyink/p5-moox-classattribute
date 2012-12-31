cabb8727357eaf1b4486f309c32e90013f6bde62
Toby Inkster <mail@tobyinkster.co.uk>
2012-12-31 15:45:58 +0000

    document examples

--- examples/hooks/track-consumers.pl
+++ examples/hooks/track-consumers.pl
@@ -1,3 +1,25 @@
+=head1 PURPOSE
+
+Demonstrate subtleties of L<MooX::CaptainHook>'s C<on_application>.
+
+=head1 EXPECTED OUTPUT
+
+ Local::Person
+ Local::Document
+
+=head1 AUTHOR
+
+Toby Inkster E<lt>tobyink@cpan.orgE<gt>.
+
+=head1 COPYRIGHT AND LICENCE
+
+This software is copyright (c) 2013 by Toby Inkster.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
 use 5.010;
 use strict;
 use warnings;
