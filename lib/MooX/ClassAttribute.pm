8fa98321144548efeb16464bf42721f6b6f5e825
Toby Inkster <mail@tobyinkster.co.uk>
2013-08-27 10:34:09 +0100

    bump version; changelog

--- lib/MooX/ClassAttribute.pm
+++ lib/MooX/ClassAttribute.pm
@@ -6,7 +6,7 @@ use warnings;
 
 BEGIN {
 	$MooX::ClassAttribute::AUTHORITY = 'cpan:TOBYINK';
-	$MooX::ClassAttribute::VERSION   = '0.008';
+	$MooX::ClassAttribute::VERSION   = '0.009';
 }
 
 use Carp;
